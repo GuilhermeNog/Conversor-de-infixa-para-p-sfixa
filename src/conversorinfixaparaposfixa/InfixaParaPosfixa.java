4318761867f3e82b77150a6d2c8b692bcb032080
Guilherme Nogueira <guilhermenogueira1616@gmail.com>
2022-11-29 19:01:31 -0300

    commit

---
+++ src/conversorinfixaparaposfixa/InfixaParaPosfixa.java
@@ -0,0 +1,19 @@
+package conversorinfixaparaposfixa;
+
+import java.util.Scanner;
+
+public class InfixaParaPosfixa {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String expressao;
+		
+		if(args.length == 0)
+		{
+			System.out.println("Informe a expressão infixa: ");
+			expressao = scanner.nextLine().replace(" ", "");
+		}
+		else
+			expressao = args[0];
+		System.out.println(Posfixa.conversao(expressao));
+	}
+}
